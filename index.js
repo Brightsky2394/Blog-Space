cc53387b186d2ce814c9136b71e5d38a4ab1bcda
Yaya <sikiruyaya1@gmail.com>
2025-07-12 11:44:06 +0100

    simple blog space project

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let blogContainer = document.getElementById('blog-container');
+let postsArr = [];
+
+const renderPosts = () => {
+    let html = '';
+        postsArr.forEach(post => {
+            html += `
+              <h1>${post.title}</h1>
+              <p>${post.body}</p>
+              <hr/>
+            `
+        });
+        blogContainer.innerHTML = html
+}
+
 const getData = async () => {
     const url = 'https://apis.scrimba.com/jsonplaceholder/posts';
     try {
@@ -7,19 +22,45 @@ const getData = async () => {
         }
         const data = await resp.json();
         console.log(data);
-        let html = '';
-        const postsArr = data.splice(0, 5);
-        postsArr.forEach(post => {
-            html += `
-              <h1>${post.title}</h1>
-              <p>${post.body}</p>
-              <hr/>
-            `
-        });
-        document.getElementById('blog-container').innerHTML = html;
+        
+        postsArr = data.splice(0, 5);
+        renderPosts();
     } catch (err) {
         console.log(err.message)
     }
 }
 
 getData();
+
+
+const formData = document.getElementById('form-data');
+
+formData.addEventListener('submit', e => {
+    e.preventDefault();
+    const titleText = document.getElementById('title').value;
+    const messageText = document.getElementById('message').value;
+    const newDataObj = {
+         title: `${titleText}`,
+         body: `${messageText}`
+    }
+   const options = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: `${titleText}`,
+        body: `${messageText}`
+    }),
+    
+}
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', options)
+      .then(resp => resp.json())
+      .then(data => {
+         console.log(data);
+         postsArr.unshift(data)
+         renderPosts();
+         formData.reset();
+      })
+       
+})
